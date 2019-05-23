4f2747be17df098d34e477b7a2391d08f4dcfbc1
Eduardo Antolín Jiménez <laloantolin@hotmail.com>
2019-05-22 22:08:14 -0700

    VideoJuegoFrizby
    Juego Topicos De Fisica FrizBY

---
+++ MateFinal/Assets/ControlNeon.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlNeon : MonoBehaviour
+{
+    [SerializeField]
+    Material mat;
+    float conteo;
+    bool gamestart;
+    void Start()
+    {
+        conteo = 3;
+        gamestart = false;
+        mat = GetComponent<Material>();
+        mat = (Material)Resources.Load("Estado_Neon", typeof(Material));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            gamestart = true;
+
+        }
+        if ( gamestart == true)
+        {
+           if (conteo >= 0.7)
+            {
+                conteo -= Time.deltaTime;
+            }
+            mat.SetFloat("_RimPower", conteo);
+        }
+    }
+}
---
+++ MateFinal/Assets/Scripts 1/Disc_Spawn.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disc_Spawn : MonoBehaviour
+{
+    [SerializeField]
+    GameObject disco;
+    [SerializeField]
+    GameObject imagen;
+    [SerializeField]
+    GameObject texto;
+    Transform trans;
+    public float scoreblue;
+    public float scorered;
+    float contar;
+    float player;
+    bool gamestart;
+    void Start()
+    {
+        gamestart = false;
+        contar = 3;
+        trans = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            gamestart = true;
+            texto.SetActive(false);
+            imagen.SetActive(false);
+            contar -= Time.deltaTime;
+            if (contar <= 0)
+            {
+               
+                    Instantiate(disco, transform.position, trans.rotation);
+
+                contar = 3;
+            }
+        }
+        
+        if(gamestart == true)
+        {
+            if (GameObject.FindGameObjectsWithTag("disco").Length == 0)
+            {
+                contar -= Time.deltaTime;
+                if (contar <= 0)
+                {
+                    Instantiate(disco, transform.position, trans.rotation);
+
+                    contar = 3;
+                }
+            }
+        }
+        
+
+    }
+}
